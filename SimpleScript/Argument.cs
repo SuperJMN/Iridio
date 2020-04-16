c0a5602636f5f88760b4c83dbbb0263f105a72ce
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-04-16 15:55:14 +0200

    Arguments for functions

---
+++ SimpleScript/Argument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleScript
+{
+    public class Argument
+    {
+        public Argument(string name, Type type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public Type Type { get; }
+        public string Name { get; }
+    }
+}
--- SimpleScript/IFunction.cs
+++ SimpleScript/IFunction.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SimpleScript
 {
@@ -6,5 +8,7 @@ namespace SimpleScript
     {
         Task<object> Invoke(object[] parameters);
         string Name { get; }
+        IEnumerable<Argument> Arguments { get; }
+        Type ReturnType { get; }
     }
 }
