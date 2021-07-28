c5462cf4848982ec29c805c8bf8a0958e0178e47
José Manuel Nieto Sánchez <superjmn@outlook.com>
2021-07-28 22:24:22 +0200

    FileSystem

---
+++ Source/Iridio/FileSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Iridio.Preprocessing;
+
+namespace Iridio
+{
+    public class FileSystem : IFileSystem
+    {
+        public string WorkingDirectory
+        {
+            get => Directory.GetCurrentDirectory();
+            set => Directory.SetCurrentDirectory(value);
+        }
+
+        public ITextFile Get(string path)
+        {
+            return new TextFile(path);
+        }
+    }
+}
---
+++ Source/Iridio/TextFile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Iridio.Preprocessing;
+
+namespace Iridio
+{
+    public class TextFile : ITextFile
+    {
+        private readonly string path;
+
+        public TextFile(string path)
+        {
+            this.path = path;
+        }
+
+        public IEnumerable<string> Lines()
+        {
+            return File.ReadLines(path);
+        }
+    }
+}
